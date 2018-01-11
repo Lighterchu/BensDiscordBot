0db04a2d9820d982f8f85a9e4b89a0edee93ac04
Flyken <jaredcollins99@gmail.com>
2018-01-11 00:44:40 -0500

    fixed welcome message

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ bot.on("ready", function() {
 
 
 bot.on("guildMemberAdd", function(member) {
-    member.guild.channels.find("name", "general").sendMessage(member.toString() + "welcome to the server!");
+    member.guild.channels.find("name", "general").sendMessage(member.toString() + ", welcome to the server!");
 
     member.addRole(member.guild.roles.find("name","test"));
 });
